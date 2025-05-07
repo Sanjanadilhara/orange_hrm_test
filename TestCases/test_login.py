bfa5b4a971e63445be83a6fc3e0a90da40e93cb6
sanjanadilhara <sanjanadilhara2000@gmail.com>
2025-05-08 00:32:53 +0530

    init

--- TestCases/test_login.py
+++ TestCases/test_login.py
@@ -6,7 +6,9 @@ import time
 class Test_001_Login:
     base_url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
     username = "Admin"
+    wrong_username = "Admin1"
     password = "admin123"
+    wrong_password = "admin1235"
 
     def test_homePageTitle(self):
         driver = webdriver.Chrome()
@@ -25,5 +27,33 @@ class Test_001_Login:
         login.clickLogin()
 
         time.sleep(3)
-        assert "OrangeHRM" in driver.title
+        assert driver.current_url == "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index"
+        driver.quit()
+
+    def test_login_wrong_password(self):
+        driver = webdriver.Chrome()
+        driver.get(self.base_url)
+        login = Login(driver)
+
+        time.sleep(10)
+        login.setUsername(self.username)
+        login.setPassword(self.wrong_password)
+        login.clickLogin()
+
+        time.sleep(5)
+        assert driver.current_url == "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+        driver.quit()
+
+    def test_login_wrong_username(self):
+        driver = webdriver.Chrome()
+        driver.get(self.base_url)
+        login = Login(driver)
+
+        time.sleep(10)
+        login.setUsername(self.wrong_username)
+        login.setPassword(self.password)
+        login.clickLogin()
+
+        time.sleep(5)
+        assert driver.current_url == "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
         driver.quit()
